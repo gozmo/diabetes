04b79f0e20e5a43081756f52dbe587ee70753672
goz <gozmo1337@gmail.com>
2015-03-07 20:51:49 +0100

    data set can be read, non-optimal

--- read_files.py
+++ read_files.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy
+import numpy as np
 import pylab
 from PIL import Image
 
@@ -7,12 +7,29 @@ from PIL import Image
 HEIGHT=3168
 WIDTH=4752
 
+def read_training_set():
+    labels = read_labels()
+    samples = read_sample_images()
+    X = []
+    y = np.array([])
+    for ((name_left, img_left),(name_right, img_right)) in samples:
+        print name_left
+        row_left = labels[labels["image"] == name_left]
+        label_left = row_left.values[0][1]
+        X.append(img_left)
+        np.append(y, label_left)
+
+        row_right = labels[labels["image"] == name_right]
+        label_right = row_right.values[0][1]
+        X.append(img_right)
+        y = np.append(y, label_right)
+    return X,y
+
 def read_labels():
-    labels = pd.read_csv("trainLabels.csv", index_col=0)
+    labels = pd.read_csv("trainLabels.csv")
     return labels
 
 def read_images():
-    #for index in xrange(44350):
     return _read_images(44350)
 
 def read_sample_images():
@@ -23,7 +40,6 @@ def _read_images(upper_limit):
         try:
             left, right = _read_file_pair("sample", index)
         except IOError as e:
-            print e
             continue
         yield left, right
 
@@ -34,8 +50,8 @@ def _read_file_pair(path, index):
     img_left = Image.open(open(filename_left))
     img_right = Image.open(open(filename_right))
 
-    img_left = numpy.asarray(img_left, dtype='float64') / 256.
-    img_right = numpy.asarray(img_right, dtype='float64') / 256.
+    img_left = np.asarray(img_left, dtype='float64') / 256.
+    img_right = np.asarray(img_right, dtype='float64') / 256.
 
     #img_left = img_left.transpose(2, 0, 1).reshape(1, 3, HEIGHT, WIDTH)
     #img_right = img_right.transpose(2, 0, 1).reshape(1, 3, HEIGHT, WIDTH)
